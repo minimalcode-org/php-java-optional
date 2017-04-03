18ee90f101b20ba9479b166ec79f94715f51267c
Fabio Piro <f.piro@miowelfare.it>
2017-04-04 00:46:03 +0200

    Added more tests

--- tests/Minimalcode/Optional/Book.php
+++ tests/Minimalcode/Optional/Book.php
@@ -4,4 +4,24 @@ namespace Minimalcode\Optional;
 
 class Book
 {
+    /**
+     * @var int
+     */
+    private $pages;
+
+    /**
+     * @return int
+     */
+    public function getPages()
+    {
+        return $this->pages;
+    }
+
+    /**
+     * @param int $pages
+     */
+    public function setPages($pages)
+    {
+        $this->pages = $pages;
+    }
 }
--- tests/Minimalcode/Optional/OptionalTest.php
+++ tests/Minimalcode/Optional/OptionalTest.php
@@ -49,4 +49,44 @@ class OptionalTest extends \PHPUnit_Framework_TestCase
         $optBook2 = OptionalBook::of(new Book());
         static::assertTrue($optBook2->isPresent());
     }
+
+    public function testIsPresentNot()
+    {
+        self::assertFalse(OptionalBook::ofEmpty()->isPresent());
+    }
+
+    public function testIfPresent()
+    {
+        $success = false;
+        OptionalBook::of(new Book())
+            ->ifPresent(function () use (&$success) {
+                $success = true;
+            });
+
+        static::assertTrue($success);
+    }
+
+    public function testIfPresentNot()
+    {
+        $success = false;
+        OptionalBook::ofEmpty()
+            ->ifPresent(function () use (&$success) {
+                $success = true;
+            });
+
+        static::assertFalse($success);
+    }
+
+    public function testFilter()
+    {
+        $book = new Book();
+        $book->setPages(5);
+
+        $optBook = OptionalBook::of($book)
+            ->filter(function(Book $b) {
+                return $b->getPages() > 5;
+            });
+
+        self::assertFalse($optBook->isPresent());
+    }
 }
